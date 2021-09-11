018f1f5f27ec4b03bdd11fe5d1cffb22b8d81f8b
Crowdbotics <team@crowdbotics.com>
2021-09-11 11:09:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "karratapp_30465Identifier",
+  url: "https://karratapp_30465Identifier.botics.co",
+  api: "https://karratapp_30465Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
